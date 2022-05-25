157988478ffe3629805f6cbeaf75b1ad90a304b1
tiaog <2021380@student.cct.ie>
2022-05-25 18:15:07 +0000

    starting the styling

--- breaking-bad-app/src/app/pages/characters/characters.page.ts
+++ breaking-bad-app/src/app/pages/characters/characters.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-characters',
--- breaking-bad-app/src/app/pages/tabs/tabs-routing.module.ts
+++ breaking-bad-app/src/app/pages/tabs/tabs-routing.module.ts
@@ -5,10 +5,53 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'characters',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+          },
+        ]
+      },
+      {
+        path: 'episodes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+          }
+        ]
+      },
+      {
+        path: 'quotes',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+          }
+        ]
+      },
+      {
+        path: 'deathcount',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../deathcount/deathcount.module').then( m => m.DeathcountPageModule)
+          }
+        ]
+      }
+    ]
+  },
+  {
     path: '',
-    component: TabsPage
-  }
-];
+    redirectTo: '/tabs/characters',
+    pathMatch: 'full'
+}]
+;
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
